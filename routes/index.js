56fba471e8f82e23da299244bf24204fe40a69bd
RdP <Rien.duPre@me.com>
2017-10-05 21:31:47 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -41,6 +41,8 @@ router.get("/status", function (req, res, next) {
     var temp = fs.readFileSync(hb.temp);
     var cputemp = ((temp/1000).toPrecision(3)) + "°C";
     
+    var teamviewerid = "Testje"
+    
     uptime.days = Math.floor(uptime.delta / 86400);
     uptime.delta -= uptime.days * 86400;
     uptime.hours = Math.floor(uptime.delta / 3600) % 24;
